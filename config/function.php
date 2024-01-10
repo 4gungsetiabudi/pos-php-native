c669a24cd65f09e88efb038912d21c0d2673ac43
4gungsetiabudi <4gungsetiabudi@gmail.com>
2024-01-11 06:02:48 +0700

    pos-php-native

---
+++ config/function.php
@@ -0,0 +1,167 @@
+<?php
+session_start();
+
+require 'dbcon.php';
+
+// Masukan kolom validasi
+function validate($inputData){
+
+    global $conn;
+    $validatedData = mysqli_real_escape_string($conn, $inputData);
+    return trim($validatedData);
+}
+
+// Redirect dari 1 halaman ke halaman lain dengan pesan (status)
+function redirect($url, $status){
+
+    $_SESSION['status'] = $status;
+    header('Location: '.$url);
+    exit(0);
+}
+
+// Tampilan pesan atau status setelah proses apa pun
+function alertMessage(){
+
+    if(isset($_SESSION['status'])){
+        echo $_SESSION['status'];
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+           <h5>'.$_SESSION['status'].'<h5>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+        unset($_SESSION['status']);
+    }
+}
+
+// Masukkan catatan menggunakan fungsi ini
+function insert($tableName, $data)
+{
+    global $conn;
+
+    $table = validate($tableName);
+
+    $columns = array_keys($data);
+    $values = array_values($data);
+
+    $finalColumn = implode(',', $columns);
+    $finalValues = "'".implode("', '", $values). "'";
+
+    $query = "INSERT INTO $table ($finalColumn) VALUES ($finalValues)";
+    $result = mysqli_query($conn,$query);
+    return $result;
+}
+
+// Update Data menggunakan fungsi tersebut
+function update($tableName, $id, $data){
+
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $updateDataString = "";
+
+    foreach($data as $column => $value){
+
+        $updateDataString .= $column.'='."'$value',";
+    }
+
+    $finalUpdateData = substr(trim($updateDataString),0,-1);
+
+    $query = "UPDATE $table SET $finalUpdateData WHERE id='$id'";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
+function getAll($tableName, $status = NULL){
+
+    global $conn;
+
+    $table = validate($tableName);
+    $status = validate($status);
+
+    if($status == 'status')
+    {
+        $query = "SELECT * FROM $table WHERE status='0'";
+    }
+    else
+    {
+        $query = "SELECT * FROM $table";
+    }
+    return mysqli_query($conn, $query);
+}
+
+function getById($tableName, $id){
+
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $query = "SELECT * FROM $table WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+
+    if($result){
+
+        if(mysqli_num_rows($result) == 1){
+
+            $row = mysqli_fetch_assoc($result);
+            $response = [
+                'status' => 200,
+                'data' => $row,
+                'message' => 'Record Found'
+            ];
+            return $response;
+
+        }else{
+
+            $response = [
+                'status' => 404,
+                'message' => 'No Data Found'
+            ];
+            return $response;
+        }
+
+    }else{    
+        $response = [
+            'status' => 500,
+            'message' => 'Something Went Wrong'
+        ];
+        return $response;
+    }
+}
+
+// Delet data dari database menggunakan ID
+function delete($tableName, $id){
+
+    global $conn;
+
+    $table = validate($tableName);
+    $id = validate($id);
+
+    $query = "DELETE FROM $table WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
+function checkParamId($type){
+
+    if(isset($_GET[$type])){
+        if($_GET[$type] != ''){
+
+            return $_GET[$type];
+        }else{
+            return '<h5>No Id Found</h5>';
+        }
+
+    }else{
+        return '<h5>No Id Given</h5>';
+    }
+}
+
+function logoutSession(){
+
+    unset($_SESSION['loggedIn']);
+    unset($_SESSION['loggedInUser']);
+}
+
+?>
